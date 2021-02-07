9f45b1b554758e0ab486e786b8fc810bec1e3ef1
Syed Adnan Mohamed <54139717+AdnanENG19CT0030@users.noreply.github.com>
2021-02-07 21:47:08 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,38 +1,39 @@
 //WAP to find the sum of two fractions.
 #include<stdio.h>
-	struct Fraction
-	{
-		int w,x,y,z,nr,dr,G;
-	}frac;
-	void inputValues()
-	{
-		printf("Enter the numerator of the first fraction:\n");
-		scanf("%d",&frac.w);
-		printf("Enter the denominator of the first fraction:\n");
-		scanf("%d",&frac.x);
-		printf("Enter the numerator of the second fraction:\n");
-		scanf("%d",&frac.y);
-		printf("Enter the denominator of the second fraction:\n");
-		scanf("%d",&frac.z);
-	}
-	void computeFraction()
-	{
-		frac.nr = w*z+y*x;
-		frac.dr = x*z
-		for( int a=1;a<=nr && a<=dr;a++)
-		{
-			if(nr%a==0 && dr%a==0)
-				frac.G = a;
-		}
-	}
-	void printFraction()
-	{
-		printf("The resulting fraction is %d / %d\n",(frac.nr/frac.G),(frac.dr/frac.G));
-	}
-	int main()
-	{
-		inputValues();
-		computeFraction();
-		printFraction();
-		return 0;
-	}
+int nr,dr,G;
+struct Fraction
+{
+  int num,den;
+}frac[2];
+void inputValues()
+{
+  printf("Enter the numerator of the first fraction:\n");
+  scanf("%d",&frac[0].num);
+  printf("Enter the denominator of the first fraction:\n");
+  scanf("%d",&frac[0].den);
+  printf("Enter the numerator of the second fraction:\n");
+  scanf("%d",&frac[1].num);
+  printf("Enter the denominator of the second fraction:\n");
+  scanf("%d",&frac[1].den);
+}
+void computeFraction()
+{
+  nr = frac[0].num*frac[1].den+frac[1].num*frac[0].den;
+  dr = frac[0].den*frac[1].den;
+  for(int a=1;a<=nr && a<=dr;a++)
+  {
+    if(nr%a==0 && dr%a==0)
+      G = a;
+  }
+}
+void printFraction()
+{
+  printf("The resultant fraction is %d / %d\n",(nr/G),(dr/G));
+}
+int main()
+{
+  inputValues();
+  computeFraction();
+  printFraction();
+  return 0;
+}
